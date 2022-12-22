887ec6e3977c5b3004db31522e2ce3c447be9cd1
iwantthatresult <120048765+iwantthatresult@users.noreply.github.com>
2022-12-22 23:41:07 +0100

    Update untitled19.py

--- untitled19.py
+++ untitled19.py
@@ -45,9 +45,10 @@ user_input5 = st.text_input(savedfiles, '')
  #   subprocess.check_call([sys.executable, '-m', 'pip', 'install', *missing])
 
 cwd=str(os.getcwd())
-user_input6=st.text_input(cwd)
-#subprocess.run(["git", "clone", "https://github.com/iwantthatresult/ytdlspleeter.git"])
+
+subprocess.run(["git", "clone", "https://github.com/iwantthatresult/ytdlspleeter.git"])
 gitdir=cwd+'ytdlspleeter'
+
 def save(fname,TOKEN):
   savecwd=cwd
   os.chdir(cwd +'/ytdlspleeter')
@@ -60,46 +61,15 @@ def save(fname,TOKEN):
   subprocess.run(['git' ,'push',  'https://iwantthatresult:'+TOKEN+'@github.com/iwantthatresult/ytdlspleeter.git'])
   os.chdir(savecwd)
 
+#--------------------------------------------------
 
+import librosa
+import numpy as np
+import pandas as pd
+import urllib.request
+from PIL import Image
+import io
 
-def youtube2mp3 (url,outdir,fname,Token):
-    # url input from user
-    yt = YouTube(url)
-
-    ##@ Extract audio with 160kbps quality from video
-    video = yt.streams.filter(only_audio=True,abr='160kbps').last()
-
-    ##@ Downloadthe file
-    out_file = video.download(output_path=outdir,filename=fname)
-    base, ext = os.path.splitext(out_file)
-    new_file = Path(f'{base}.mp3')
-    os.rename(out_file, new_file)
-    ##@ Check success of download
-    if new_file.exists():
-        print(f'{yt.title} has been successfully downloaded.')
-        idsave=fname
-        fnamesave=fname+'.mp3'
-        fext=cwd+"/audio/"+fname+'/'
-        fname=cwd+"/audio/"+fname+'/'+fname+'.mp3'
-        out=cwd+'/audio/'
-        subprocess.run(["spleeter", "separate", fname ,"-p" "spleeter:5stems", "-c", "mp3", "-o", out], capture_output=True)
-        dfinfo=ytdata(url)
-        df1=extract_features_orig(fname)
-        df2=extract_features_spleeted(fext+'vocals.mp3','vocals')
-        df3=extract_features_spleeted(fext+'drums.mp3','drums')
-        df4=extract_features_spleeted(fext+'piano.mp3','other')
-        df5=extract_features_spleeted(fext+'piano.mp3','piano')
-        df6=extract_features_spleeted(fext+'bass.mp3','bass')
-        df=pd.concat([dfinfo,df1,df2,df3,df4,df5,df6],axis=0)
-        df.to_csv(fext+idsave+".csv", index=False)
-        audio_file = open(fname, 'rb')
-        audio_bytes = audio_file.read()
-        st.audio(audio_bytes, format='mp3')
-        user_input=st.text_input(fname)
-        save(idsave,Token)
-    else:
-        print(f'ERROR: {yt.title}could not be downloaded!')
-        
 def extract_features_orig(file_path,total=False):
   # Load the audio file
   audio, sample_rate = librosa.load(file_path)
@@ -195,17 +165,60 @@ def ytdata(url):
   img=arraysavefromurl(video.thumbnail_url)
   df=pd.DataFrame(data=[[id,title,description,keywords,duration,views,meta]],columns=['id','Title','Description','keywords','duration','views','meta'])
   return df
+
+  #--------------------------------------------------
+
+
+def youtube2mp3 (url,outdir,fname,Token):
+    # url input from user
+    yt = YouTube(url)
+
+    ##@ Extract audio with 160kbps quality from video
+    video = yt.streams.filter(only_audio=True,abr='160kbps').last()
+
+    ##@ Downloadthe file
+    out_file = video.download(output_path=outdir,filename=fname)
+    base, ext = os.path.splitext(out_file)
+    new_file = Path(f'{base}.mp3')
+    os.rename(out_file, new_file)
+    ##@ Check success of download
+    if new_file.exists():
+        print(f'{yt.title} has been successfully downloaded.')
+        idsave=fname
+        fnamesave=fname+'.mp3'
+          #--------------------------------------------------
+        fext=cwd+"/audio/"+fname+'/'
+          #--------------------------------------------------
+        fname=cwd+"/audio/"+fname+'/'+fname+'.mp3'
+        out=cwd+'/audio/'
+        subprocess.run(["spleeter", "separate", fname ,"-p" "spleeter:5stems", "-c", "mp3", "-o", out], capture_output=True)
+
+
+        #--------------------------------------------------
+        dfinfo=ytdata(url)
+        df1=extract_features_orig(fname)
+        df2=extract_features_spleeted(fext+'vocals.mp3','vocals')
+        df3=extract_features_spleeted(fext+'drums.mp3','drums')
+        df4=extract_features_spleeted(fext+'piano.mp3','other')
+        df5=extract_features_spleeted(fext+'piano.mp3','piano')
+        df6=extract_features_spleeted(fext+'bass.mp3','bass')
+        df=pd.concat([dfinfo,df1,df2,df3,df4,df5,df6],axis=0)
+        df.to_csv(fext+idsave+".csv", index=False)
+        #----------------------------------------------------------
+        save(idsave,Token)
+    else:
+        print(f'ERROR: {yt.title}could not be downloaded!')
     
 
 def audiodl(id):
   id=str.split(id)
   print(id)
   Token=id[0]
-  user_input10=st.text_input('ok')
   for i in range(1,len(id)):
-    user_input15=st.text_input('ok2')
     url='www.youtube.com/watch?v='+id[i]
     youtube2mp3(url,cwd+'/audio/'+str(id[i])+"",str(id[i]),Token)  
+
+
 
 user_input1 = st.text_input("insère ton lien poto", '')
 while len(user_input1)==0:
