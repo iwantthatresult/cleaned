63527c872fb88d318c5c03f9880c306b2425d300
iwantthatresult <120048765+iwantthatresult@users.noreply.github.com>
2022-12-22 21:42:57 +0100

    Update untitled19.py

--- untitled19.py
+++ untitled19.py
@@ -3,17 +3,17 @@ import subprocess
 import pkg_resources
 
 
-subprocess.run([sys.executable,'-m', 'pip', 'install','ffmpeg','librosa','pytube', 'gdown','spleeter','streamlit','pydrive'])
+#subprocess.run([sys.executable,'-m', 'pip', 'install','ffmpeg','librosa','pytube', 'gdown','spleeter','streamlit','pydrive'])
 
-subprocess.run([sys.executable,"-m", 'apt' ,'install' ,'ffmpeg','librosa','pytube', 'gdown','spleeter','streamlit','pydrive'])
+#subprocess.run([sys.executable,"-m", 'apt' ,'install' ,'ffmpeg','librosa','pytube', 'gdown','spleeter','streamlit','pydrive'])
 
-required  = {'pytube', 'gdown','spleeter','streamlit','pydrive','librosa'} 
-installed = {pkg.key for pkg in pkg_resources.working_set}
-missing   = required - installed
+#required  = {'pytube', 'gdown','spleeter','streamlit','pydrive','librosa'} 
+#installed = {pkg.key for pkg in pkg_resources.working_set}
+#missing   = required - installed
 
-if missing:
+#if missing:
     # implement pip as a subprocess:
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', *missing])
+ #   subprocess.check_call([sys.executable, '-m', 'pip', 'install', *missing])
 
 
 
