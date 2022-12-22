3a4a4dbf3f8dfd13873bbb2c5b89e8712867ee13
iwantthatresult <120048765+iwantthatresult@users.noreply.github.com>
2022-12-22 20:07:10 +0100

    Update untitled19.py

--- untitled19.py
+++ untitled19.py
@@ -2,9 +2,9 @@ import sys
 import subprocess
 import pkg_resources
 
-subprocess.run([sys.executable,"-m", 'apt' ,'install' ,'ffmpeg','google-api-python-client', 'google-auth-httplib2','google-auth-oauthlib'])
+subprocess.run([sys.executable,"-m", 'apt' ,'install' ,'ffmpeg',])
 
-required  = {'pytube', 'gdown','spleeter','streamlit','pydrive'} 
+required  = {'pytube', 'gdown','spleeter','streamlit','pydrive','librosa'} 
 installed = {pkg.key for pkg in pkg_resources.working_set}
 missing   = required - installed
 
@@ -23,12 +23,17 @@ import os
 from pathlib import Path
 import subprocess
 import streamlit as st
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
 from zipfile import ZipFile
-import os
 from os.path import basename
 import time
+import spleeter
+from pathlib import Path
+import librosa
+import numpy as np
+import pandas as pd
+import urllib.request
+from PIL import Image
+import io
 
 cwd=str(os.getcwd())
 
@@ -66,9 +71,19 @@ def youtube2mp3 (url,outdir,fname,Token):
         print(f'{yt.title} has been successfully downloaded.')
         idsave=fname
         fnamesave=fname+'.mp3'
+        fext=cwd+"/audio/"+fname+'/'
         fname=cwd+"/audio/"+fname+'/'+fname+'.mp3'
         out=cwd+'/audio/'
         subprocess.run(["spleeter", "separate", fname ,"-p" "spleeter:5stems", "-c", "mp3", "-o", out], capture_output=True)
+        dfinfo=ytdata(url)
+        df1=extract_features_orig(fname)
+        df2=extract_features_spleeted(fext+'vocals.mp3','vocals')
+        df3=extract_features_spleeted(fext+'drums.mp3','drums')
+        df4=extract_features_spleeted(fext+'piano.mp3','other')
+        df5=extract_features_spleeted(fext+'piano.mp3','piano')
+        df6=extract_features_spleeted(fext+'bass.mp3','bass')
+        df=pd.concat([dfinfo,df1,df2,df3,df4,df5,df6],axis=0)
+        df.to_csv(fext+idsave+".csv", index=False)
         audio_file = open(fname, 'rb')
         audio_bytes = audio_file.read()
         st.audio(audio_bytes, format='mp3')
@@ -76,6 +91,102 @@ def youtube2mp3 (url,outdir,fname,Token):
         save(idsave,Token)
     else:
         print(f'ERROR: {yt.title}could not be downloaded!')
+        
+def extract_features_orig(file_path,total=False):
+  # Load the audio file
+  audio, sample_rate = librosa.load(file_path)
+  audio, _ = librosa.effects.trim(audio, top_db=60)
+    # Extract 17 features using librosa
+  features=[]
+  features.append([audio])
+  features.append(librosa.beat.tempo(audio, sample_rate)[0])
+  features.append(librosa.feature.chroma_stft(audio, sample_rate))
+  features.append(librosa.feature.chroma_cqt(audio, sample_rate))
+  features.append(librosa.feature.chroma_cens(audio, sample_rate))
+  features.append(librosa.feature.mfcc(audio, sample_rate))
+  features.append(librosa.feature.spectral_centroid(audio, sample_rate))
+  features.append(librosa.feature.spectral_bandwidth(audio, sample_rate))
+  features.append(librosa.feature.spectral_rolloff(audio, sample_rate))
+  features.append(librosa.feature.spectral_contrast(audio, sample_rate))
+  features.append(librosa.feature.tonnetz(audio, sample_rate))
+  features.append(librosa.feature.zero_crossing_rate(audio))
+  features.append(librosa.feature.poly_features(audio, sample_rate))
+  features.append(librosa.feature.tempogram(audio, sample_rate))
+  features.append(librosa.feature.spectral_flatness(audio))
+  features.append(librosa.beat.beat_track(audio, sample_rate)[1])
+  df = pd.DataFrame(data=[features], columns=['audio','BPM','Chromastft', 'Chromacqt', 'Chromacens', 'mfccs', 'spectralcentroid', 'spectral_bandwith', 'spectral_rolloff', 'spectral_contrast', 'tonnetz',
+                                     'zero_crossing_rate', 'poly_features', 'tempogram', 'spectral flattness','beatframe'])
+  # Return the audio and extracted features
+  return df
+
+def extract_features_spleeted(file_path,spleet):
+  # Load the audio file
+  audio, sample_rate = librosa.load(file_path)
+    # Extract 17 features using librosa
+  features=[]
+  features.append([audio])
+  features.append(librosa.feature.chroma_stft(audio, sample_rate))
+  features.append(librosa.feature.chroma_cqt(audio, sample_rate))
+  features.append(librosa.feature.chroma_cens(audio, sample_rate))
+  features.append(librosa.feature.mfcc(audio, sample_rate))
+  features.append(librosa.feature.spectral_centroid(audio, sample_rate))
+  features.append(librosa.feature.spectral_bandwidth(audio, sample_rate))
+  features.append(librosa.feature.spectral_rolloff(audio, sample_rate))
+  features.append(librosa.feature.spectral_contrast(audio, sample_rate))
+  features.append(librosa.feature.tonnetz(audio, sample_rate))
+  features.append(librosa.feature.zero_crossing_rate(audio))
+  features.append(librosa.feature.poly_features(audio, sample_rate))
+  features.append(librosa.feature.spectral_flatness(audio))
+  if spleet=='drums':
+    features.append(librosa.feature.tempogram(audio, sample_rate))
+    features.append(librosa.beat.beat_track(audio, sample_rate)[0])
+    features.append(librosa.beat.beat_track(audio, sample_rate)[1])
+    df = pd.DataFrame(data=[features], columns=['audio '+spleet,'Chromastft ' +spleet, 'Chromacqt ' +spleet, 'Chromacens ' +spleet, 'mfccs ' +spleet, 'spectralcentroid ' +spleet, 'spectral_bandwith ' +spleet, 'spectral_rolloff ' +spleet, 'spectral_contrast ' +spleet, 'tonnetz ' +spleet,
+                                     'zero_crossing_rate' +spleet, 'poly_features' +spleet, 'tempogram' +spleet, 'spectral flattness' +spleet,'BPM ' +spleet ,'beatframe' +spleet])
+
+
+  else:
+    df = pd.DataFrame(data=[features], columns=['audio '+spleet,'Chromastft ' +spleet, 'Chromacqt ' +spleet, 'Chromacens ' +spleet, 'mfccs ' +spleet, 'spectralcentroid ' +spleet, 'spectral_bandwith ' +spleet, 'spectral_rolloff ' +spleet, 'spectral_contrast ' +spleet, 'tonnetz ' +spleet,
+                                     'zero_crossing_rate' +spleet, 'poly_features' +spleet, 'spectral flattness' +spleet])
+
+
+
+  # Return the extracted features and BPM
+  return df
+
+
+def remplacer_caracteres(chaine, ancien_caractere, nouveau_caractere):
+  # Utiliser la méthode replace() de la classe str pour remplacer les occurences
+  # de l'ancien caractère par le nouveau
+  nouvelle_chaine = chaine.replace(ancien_caractere, nouveau_caractere)
+  return nouvelle_chaine
+
+def arraysavefromurl(url):
+  # Open the URL and read the image data
+  with urllib.request.urlopen(url) as url:
+    image_data = url.read()
+  # Create a file-like object from the image data
+  image_file = io.BytesIO(image_data)
+  # Open the image using the PIL library
+  image = Image.open(image_file)
+  # Convert the image to a NumPy array
+  image_array = np.array(image)
+  return image_array
+
+def ytdata(url):
+  video = YouTube(url)
+  title=video.title
+  title=remplacer_caracteres(title, '\n', ' ')  
+  description=video.description
+  description=remplacer_caracteres(description, '\n', ' ')
+  keywords=video.keywords
+  duration=video.length
+  views=video.views
+  meta=video.metadata
+  id=video.video_id
+  img=arraysavefromurl(video.thumbnail_url)
+  df=pd.DataFrame(data=[[id,title,description,keywords,duration,views,meta]],columns=['id','Title','Description','keywords','duration','views','meta'])
+  return df
     
 
 def audiodl(id):
