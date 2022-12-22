1bc16de655dcb35a28d896ae7a0d637a62acd5d5
iwantthatresult <120048765+iwantthatresult@users.noreply.github.com>
2022-12-22 23:01:47 +0100

    Update untitled19.py

--- untitled19.py
+++ untitled19.py
@@ -23,24 +23,19 @@ import io
 
 
 #subprocess.run([sys.executable,'-m', 'pip', 'install','ffmpeg','librosa','pytube', 'gdown','spleeter','streamlit','pydrive'])
-subprocess.run([sys.executable,'-ls'])
 #subprocess.run([sys.executable,"-m", 'apt' ,'install' ,'ffmpeg','librosa','pytube', 'gdown','spleeter','streamlit','pydrive'])
 
-required  = {'pytube', 'gdown','spleeter','streamlit','pydrive','librosa'} 
-installed = {pkg.key for pkg in pkg_resources.working_set}
-missing   = required - installed
+#required  = {'pytube', 'gdown','spleeter','streamlit','pydrive','librosa'} 
+#installed = {pkg.key for pkg in pkg_resources.working_set}
+#missing   = required - installed
 
-if missing:
+#if missing:
     # implement pip as a subprocess:
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', *missing])
-
-user_input4 = st.text_input("ok1", '')
+ #   subprocess.check_call([sys.executable, '-m', 'pip', 'install', *missing])
 
 cwd=str(os.getcwd())
-user_input2 = st.text_input("ok2", '')
 #subprocess.run(["git", "clone", "https://github.com/iwantthatresult/ytdlspleeter.git"])
 gitdir=cwd+'ytdlspleeter'
-user_input3 = st.text_input("ok3", '')
 def save(fname,TOKEN):
   savecwd=cwd
   os.chdir(cwd +'/ytdlspleeter')
