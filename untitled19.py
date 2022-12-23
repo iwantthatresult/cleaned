307defdfffb3b272089a7142505fe2c889bef2bb
iwantthatresult <120048765+iwantthatresult@users.noreply.github.com>
2022-12-23 23:04:33 +0100

    Update untitled19.py

--- untitled19.py
+++ untitled19.py
@@ -20,9 +20,9 @@ import pandas as pd
 import urllib.request
 from PIL import Image
 import io
-subprocess.run([sys.executable,'-m','sudo', 'apt', 'install', 'ffmpeg'])
+subprocess.run([sys.executable,'-m','sudo', 'apt', 'install', 'ffmpeg','pydub'])
 
-subprocess.run([sys.executable,'apt''-get' 'install' 'ffmpeg'])
+subprocess.run([sys.executable,'apt','-get' ,'install', 'ffmpeg'])
 
 
 # Get the list of all files in the directory
